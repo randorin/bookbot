8e97699f5825cc87b3fb1689146307fd013964e3
randorin <erinseaweed@gmail.com>
2024-06-20 16:32:02 -0400

    attempt at sorting key in a dictionary

--- main.py
+++ main.py
@@ -32,8 +32,29 @@ def count_chars(path):
                     char_count[lower_char] += 1
                 else:
                     char_count[lower_char] = 1
-            result_dict = dict(char_count)
-        print(result_dict)
+            global char_dict
+            char_dict = dict(char_count)
+        print(char_dict)
+
+def sort_on():
+    for key in char_dict:
+        if key.isalpha():
+            sorted(key)
+            print (f"The character: {color.BOLD} {key} {color.END} was found {color.BOLD} {char_dict[key]} {color.END} times.")
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
 main()
 count_words("books/frankenstein.txt")
 count_chars("books/frankenstein.txt")
+sort_on()
